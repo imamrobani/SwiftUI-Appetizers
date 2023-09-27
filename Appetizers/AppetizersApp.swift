9b168fbbfe5df95bf1eb0c35c4ae2520c892cb54
Imam Robani <imamrob@gmail.com>
2023-09-27 18:37:24 +0700

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Imam Robani on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
